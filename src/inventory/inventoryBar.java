6c91aff1df537acac0d837d107afc7bc2d5277d1
bradfordryan <bradfordryan@MHS-CAD-13.MELROSESCHOOLS.local>
2014-12-19 09:40:07 -0500

    Beta 0.4.0.2
    Created the basics basics framework for the inventory.

---
+++ src/inventory/inventoryBar.java
@@ -0,0 +1,16 @@
+package inventory;
+
+import java.awt.Graphics;
+
+import javax.swing.JPanel;
+
+public class inventoryBar extends JPanel{
+	public inventoryBar() {
+		
+	}
+	
+	@Override
+	protected void paintComponent(Graphics g) {
+		
+	}
+}
