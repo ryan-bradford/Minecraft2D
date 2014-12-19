0509687b8602e234e6c9c0f03b559510d23a5bc6
bradfordryan <bradfordryan@MHS-CAD-13.MELROSESCHOOLS.local>
2014-12-19 09:13:01 -0500

    Beta 0.3.0
    The official bug fix update!
    
    Change Log:
    Fixed most initilization bugs with NullPointerException errors.
    Fixed the bug where closing the window did not close the program.
    Worked to make the program easily changable and modular.
    Added a mid body colision point.
    Changed the look of the selector block.
    Changed the player height.

--- src/main/main.java
+++ src/main/main.java
@@ -19,7 +19,7 @@ public class main {
 	private static ArrayList<block> blocks;
 	public static int blockHeight = 64;
 
-	public static void main(String[] args ) {
+	public static void main(String[] args) {
 		screenWidth = Toolkit.getDefaultToolkit().getScreenSize().width;
 		screenHeight = Toolkit.getDefaultToolkit().getScreenSize().height;
 		map = new map(false, blockHeight);
@@ -31,70 +31,78 @@ public class main {
 
 	public static ArrayList<block> getBlocks(int height) {
 		try {
-		blocks = new ArrayList<block>();
-		for (int i = 0; i < (map.mapWidth); i++) {
-			blocks.add(map.getBlock(height, i));
+			try {
+				blocks = new ArrayList<block>();
+				for (int i = 0; i < (map.mapWidth); i++) {
+					blocks.add(map.getBlock(height, i));
+				}
+				return blocks;
+			} catch (NullPointerException ex) {
+				return new ArrayList<block>();
+			}
+		} catch (ArrayIndexOutOfBoundsException ex) {
+			return new ArrayList<block>();
 		}
-		return blocks;
-		} catch(ArrayIndexOutOfBoundsException ex) {
-			
-		}
-		return null;
 	}
-	
+
 	public static Boolean getCreative() {
 		return map.creative;
 	}
-	
+
 	public static player getPlayer() {
 		try {
-		return map.player;
-		} catch(NullPointerException ex) {
+			return map.player;
+		} catch (NullPointerException ex) {
 			return new player();
 		}
 	}
-	
+
 	public static int getWalkSpeed() {
 		return map.walkSpeed;
 	}
-	
+
 	public static int getBlockHeight() {
 		return blockHeight;
 	}
-	
+
 	public static void movePlayer(int x, int y) {
-		map.player.setBounds(map.player.getBounds().x + x,
-				(map.player.getBounds().y) + y,
-				map.player.getBounds().width,
-				map.player.getBounds().height);
+		try {
+			map.player
+					.setBounds(map.player.getBounds().x + x,
+							(map.player.getBounds().y) + y,
+							map.player.getBounds().width,
+							map.player.getBounds().height);
+		} catch (NullPointerException ex) {
+
+		}
 	}
-	
+
 	public static jump getJumpingObject() {
 		return map.jump;
 	}
-	
+
 	public static void startjumping() {
 		map.jump = new jump();
 		map.jump.start();
 		map.jumping = true;
 	}
-	
+
 	public static Boolean getJumping() {
 		try {
-		return map.jumping;
-		} catch(NullPointerException ex) {
+			return map.jumping;
+		} catch (NullPointerException ex) {
 			return false;
 		}
 	}
-	
+
 	public static void doneJumping() {
 		map.doneJumping();
 	}
-	
+
 	public static void setJumpNull() {
 		map.jump = null;
 	}
-	
+
 	public static double getJumpDistance() {
 		return map.jumpDistance;
 	}
@@ -102,80 +110,79 @@ public class main {
 	public static double getJumpSpeed() {
 		return map.jumpSpeed;
 	}
-	
+
 	public static Boolean getCollisionTop() {
 		return map.getCollisionTop();
 	}
-	
+
 	public static Boolean getCollisionBottom() {
 		return map.getCollisionBottom();
 	}
-	
+
 	public static Boolean getCollisionLeft() {
 		return map.getCollisionLeft();
 	}
-	
+
 	public static Boolean getCollisionRight() {
 		return map.getCollisionRight();
 	}
-	
+
 	public static movePlayer getMoveLeft() {
 		return map.moveLeft;
 	}
-	
+
 	public static void startMoveLeft() {
 		map.moveLeft = new movePlayer(true, null);
 		map.moveLeft.start();
 	}
-	
+
 	public static void stopMoveLeft() {
 		map.moveLeft.stop();
 		map.moveLeft = null;
 	}
-	
+
 	public static movePlayer getMoveRight() {
 		return map.moveRight;
 	}
-	
-	
+
 	public static void startMoveRight() {
 		map.moveRight = new movePlayer(false, null);
 		map.moveRight.start();
 	}
-	
+
 	public static void stopMoveRight() {
 		map.moveRight.stop();
 		map.moveRight = null;
 	}
-	
+
 	public static movePlayer getMoveDown() {
 		return map.moveDown;
 	}
-	
+
 	public static void startMoveDown() {
 		map.moveDown = new movePlayer(null, false);
 		map.moveDown.start();
 	}
-	
+
 	public static void stopMoveDown() {
 		map.moveDown.stop();
 		map.moveDown = null;
 	}
-	
+
 	public static movePlayer getMoveUp() {
 		return map.moveUp;
 	}
-	
+
 	public static void startMoveUp() {
 		map.moveUp = new movePlayer(null, true);
 		map.moveUp.start();
 	}
-	
+
 	public static void stopMoveUp() {
 		map.moveUp.stop();
 		map.moveUp = null;
 	}
-	
+
 	public static void moveSelectorBlock(int xCord, int yCord) {
 		map.select.setBounds(xCord, yCord, map.blockHeight, map.blockHeight);
 	}
@@ -184,15 +191,16 @@ public class main {
 		map.drawNewBlock(xCord, row, fileName);
 		map.repaint();
 	}
-	
+
 	public static void placeBlockAtMouse(String fileName) {
-		placeBlock(map.selectThread.selectorX, map.selectThread.selectorRow, fileName);
+		placeBlock(map.selectThread.selectorX, map.selectThread.selectorRow,
+				fileName);
 	}
 
 	public static int getGravitySpeed() {
 		try {
-		return map.gravitySpeed;
-		} catch(NullPointerException ex) {
+			return map.gravitySpeed;
+		} catch (NullPointerException ex) {
 			return 64;
 		}
 	}
