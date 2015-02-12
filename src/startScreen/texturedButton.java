b6d6e269773e832e7f94796774d8a49ad4e7eb0a
bradfordryan <bradfordryan@MHS-CAD-13.MELROSESCHOOLS.local>
2015-02-12 10:27:27 -0500

    Beta 0.6.7.7
    Added a texture to the buttons in the start screen, could not figure out
    how to get them to repaint when pressed.

--- src/startScreen/texturedButton.java
+++ src/startScreen/texturedButton.java
@@ -4,34 +4,37 @@ import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
 import javax.swing.JButton;
 
 public class texturedButton extends JButton {
-	
+
 	int width;
 	int height;
 	String text;
-	
+	Graphics g1;
+	private BufferedImage image;
+	public java.io.File f = null;
+
 	public texturedButton(String text1, int width1, int height1) {
+		f = new java.io.File("textures/button.png");
+		try {
+			image = ImageIO.read(f);
+		} catch (IOException ex) {
+			// handle exception...
+		}
 		width = width1;
 		height = height1;
 		text = text1;
 	}
-	
-	@Override
+
 	protected void paintComponent(Graphics g) {
-		Graphics2D g2d = (Graphics2D) g;
-		g2d.setColor(Color.gray);
-		g2d.fillRect(0, 0, width, height);
-		g2d.setColor(Color.WHITE);
-		g2d.drawString(text, (int)(width/2 - text.length() * 3), height/2);
-		g2d.setColor(Color.DARK_GRAY);
-		g2d.setStroke(new BasicStroke(5));
-		g2d.drawLine(0, 0, 0, height);
-		g2d.drawLine(0, width, height, height);
-		g2d.drawLine(width, width, 0, height);
-		g2d.drawLine(0, width, 0, 0);
+		g.drawImage(image, 0, 0, null); // see javadoc for more info on the parameters
+		g.setColor(Color.WHITE);
+		g.drawString(text, (int) (width / 2 - text.length() * 3), height / 2);
 	}
 
 }
