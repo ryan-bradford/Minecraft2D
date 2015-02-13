783ead4f41c425d52ba8a152856d05f408351219
bradfordryan <bradfordryan@MHS-CAD-13.MELROSESCHOOLS.local>
2015-02-13 09:46:21 -0500

    Beta 0.6.7.9
    Added a back button to the start screen.

--- src/startScreen/startScreen.java
+++ src/startScreen/startScreen.java
@@ -23,6 +23,7 @@ public class startScreen {
 	texturedButton loadGame;
 	texturedButton[][] selectGame;
 	texturedButton done;
+	texturedButton back;
 	JTextPane message;
 	JTextPane enterWorldName;
 	JTextPane noSavedGames;
@@ -30,13 +31,21 @@ public class startScreen {
 	String[] savedGames;
 	SimpleAttributeSet center;
 	map map1;
-	Style s;      
+	Style s;
 	ImageIcon icUf;
+	int stage; // Stage 1 is the first screen, 2 is load game, 3 is new game
 
 	public startScreen() {
-		map1 = new map(false, 64, main.dirtHeightInBlocks, main.inventoryBlockNumber, main.inventoryGap, main.inventoryExtra, main.inventoryHeight, main.defaultBoxColor, main.swapBoxColor,
-				main.selectedBoxColor, main.backgroundColor, main.textColor, main.airColor, main.skinColor, main.pantsColor, main.shirtColor, main.shoeColor, main.imageFileNames, main.stackHeight,
-				main.jumpDistance, main.jumpSpeed, main.gravitySpeed, main.walkSpeed, main.mineBlockSpeed, null, null, null, null, 0, "normal", main.worldSeed, true, 0, 0);
+		map1 = new map(false, 64, main.dirtHeightInBlocks,
+				main.inventoryBlockNumber, main.inventoryGap,
+				main.inventoryExtra, main.inventoryHeight,
+				main.defaultBoxColor, main.swapBoxColor, main.selectedBoxColor,
+				main.backgroundColor, main.textColor, main.airColor,
+				main.skinColor, main.pantsColor, main.shirtColor,
+				main.shoeColor, main.imageFileNames, main.stackHeight,
+				main.jumpDistance, main.jumpSpeed, main.gravitySpeed,
+				main.walkSpeed, main.mineBlockSpeed, null, null, null, null, 0,
+				"normal", main.worldSeed, true, 0, 0);
 		map1.pack();
 		map1.setBounds(0, 0, main.screenWidth, main.screenHeight);
 		map1.setVisible(true);
@@ -47,85 +56,32 @@ public class startScreen {
 		StyleConstants.setAlignment(center, StyleConstants.ALIGN_CENTER);
 		map1.setLayout(null);
 		savedGames = getSavedStuff.getWorldNames();
-		newGame = new texturedButton("New/Overwrite Game", 200, 50);
-		newGame.setBounds(main.screenWidth / 2 - 300, 300, 200, 50);
-		map1.add(newGame, 0);
-		newGame.addActionListener(new listenToNewGame());
-		loadGame = new texturedButton("Load Game", 200, 50);
-		loadGame.setBounds(main.screenWidth / 2 + 100, 300, 200, 50);
-		map1.add(loadGame, 0);
-		loadGame.addActionListener(new listenToLoadGame());
-		map1.repaint();
+		startLoadScreen();
 	}
 
 	public class listenToNewGame implements ActionListener {
 		@Override
 		public void actionPerformed(ActionEvent e) {
-			newOrOld = true;
-			initDone();
-			newGame.setVisible(false);
-			loadGame.setVisible(false);
-			map1.remove(newGame);
-			map1.remove(loadGame);
-			message = new JTextPane();
-			message.setText("Type the Name of Your World");
-			message.setBounds(main.screenWidth / 2 - 100, main.screenHeight / 2 - 200, 200, 18);
-			message.setParagraphAttributes(center, true);
-			map1.add(message);
-			enterWorldName = new JTextPane();
-			enterWorldName.setBounds(main.screenWidth / 2 - 100, main.screenHeight / 2 - 100, 200, 18);
-			enterWorldName.setParagraphAttributes(center, true);
-			map1.add(enterWorldName);
-			map1.repaint();
+			newGameButton();
 		}
 	}
 
 	public class listenToLoadGame implements ActionListener {
 		@Override
 		public void actionPerformed(ActionEvent e) {
-			newGame.setVisible(false);
-			loadGame.setVisible(false);
-			map1.remove(newGame);
-			map1.remove(loadGame);
-			savedGames = getSavedStuff.getWorldNames();
-			if (savedGames == null) {
-				noSavedGames = new JTextPane();
-				noSavedGames.setText("You have no saved games");
-				noSavedGames.setBounds(main.screenWidth / 2 - 100, 50, 200, 25);
-				noSavedGames.setParagraphAttributes(center, true);
-				map1.add(noSavedGames);
-				newOrOld = true;
-				initDone();
-				message = new JTextPane();
-				message.setText("Type the Name of Your World");
-				message.setBounds(main.screenWidth / 2 - 100, main.screenHeight / 2 - 200, 200, 25);
-				message.setParagraphAttributes(center, true);
-				map1.add(message);
-				enterWorldName = new JTextPane();
-				enterWorldName.setBounds(main.screenWidth / 2 - 100, main.screenHeight / 2 - 100, 200, 25);
-				enterWorldName.setParagraphAttributes(center, true);
-				map1.add(enterWorldName);
-				map1.repaint();
-			} else {
-				int widthInButtons = main.screenWidth / 250;
-				System.out.println(widthInButtons);
-				int heightInButtons = main.screenHeight / 50;
-				int rowsNeeded = (savedGames.length / widthInButtons) + 1;
-				int counter = 0;
-				selectGame = new texturedButton[widthInButtons][heightInButtons];
-				for (int i = 0; i < rowsNeeded; i++) {
-					for (int x = 0; x < widthInButtons; x++) {
-						if (!(counter + 1 > savedGames.length)) {
-							selectGame[x][i] = new texturedButton(savedGames[counter].substring(0, savedGames[counter].length() - 4), 200, 50);
-							selectGame[x][i].setBounds(250 * x + 50, 100 * i + 50, 200, 50);
-							selectGame[x][i].addActionListener(new listenToGameButtons(counter));
-							map1.add(selectGame[x][i], 0);
-							map1.repaint();
-							counter++;
-						}
-					}
-				}
-				map1.repaint();
+			loadGameButton();
+		}
+	}
+
+	public class listenToBack implements ActionListener {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			if (stage == 1) {
+
+			} else if (stage == 2) {
+				startLoadScreen();
+			} else if (stage == 3) {
+				startLoadScreen();
 			}
 		}
 	}
@@ -139,32 +95,159 @@ public class startScreen {
 
 		@Override
 		public void actionPerformed(ActionEvent e) {
-			main.startGame(savedGames[id]);
-			map1.setVisible(false);
-			map1.setFocusable(false);
-			map1 = null;
+			pressGameButton(id);
 		}
 	}
 
 	public class listenToDone implements ActionListener {
 		@Override
 		public void actionPerformed(ActionEvent e) {
-			File thisFile = new File(enterWorldName.getText() + ".xml");
-			thisFile.delete();
-	 		main.startGame(enterWorldName.getText() + ".xml");
-			map1.setVisible(false);
-			map1.setFocusable(false);
-			map1 = null;
+			runDone();
 		}
+	}
+
+	public void pressGameButton(int id) {
+		main.startGame(savedGames[id]);
+		map1.setVisible(false);
+		map1.setFocusable(false);
+		map1 = null;
+	}
+
+	public void runDone() {
+		File thisFile = new File(enterWorldName.getText() + ".xml");
+		thisFile.delete();
+		main.startGame(enterWorldName.getText() + ".xml");
+		map1.setVisible(false);
+		map1.setFocusable(false);
+		map1 = null;
 	}
 
 	public void initDone() {
 		done = new texturedButton("Done", 200, 50);
-		done.setBounds(main.screenWidth / 2 - 100, main.screenHeight / 2 - 300, 200, 50);
+		done.setBounds(main.screenWidth / 2 - 100, main.screenHeight / 2 - 300,
+				200, 50);
 		map1.add(done);
 		done.addActionListener(new listenToDone());
 	}
-	
 
+	public void loadGameButton() {
+		newGame.setVisible(false);
+		loadGame.setVisible(false);
+		map1.remove(newGame);
+		map1.remove(loadGame);
+		savedGames = getSavedStuff.getWorldNames();
+		if (savedGames == null) {
+			noSavedGames = new JTextPane();
+			noSavedGames.setText("You have no saved games");
+			noSavedGames.setBounds(main.screenWidth / 2 - 100, 50, 200, 25);
+			noSavedGames.setParagraphAttributes(center, true);
+			map1.add(noSavedGames);
+			newOrOld = true;
+			initDone();
+			message = new JTextPane();
+			message.setText("Type the Name of Your World");
+			message.setBounds(main.screenWidth / 2 - 100,
+					main.screenHeight / 2 - 200, 200, 25);
+			message.setParagraphAttributes(center, true);
+			map1.add(message);
+			enterWorldName = new JTextPane();
+			enterWorldName.setBounds(main.screenWidth / 2 - 100,
+					main.screenHeight / 2 - 100, 200, 25);
+			enterWorldName.setParagraphAttributes(center, true);
+			map1.add(enterWorldName);
+			map1.repaint();
+		} else {
+			int widthInButtons = main.screenWidth / 250;
+			System.out.println(widthInButtons);
+			int heightInButtons = main.screenHeight / 50;
+			int rowsNeeded = (savedGames.length / widthInButtons) + 1;
+			int counter = 0;
+			selectGame = new texturedButton[widthInButtons][heightInButtons];
+			for (int i = 0; i < rowsNeeded; i++) {
+				for (int x = 0; x < widthInButtons; x++) {
+					if (!(counter + 1 > savedGames.length)) {
+						selectGame[x][i] = new texturedButton(
+								savedGames[counter].substring(0,
+										savedGames[counter].length() - 4), 200,
+								50);
+						selectGame[x][i].setBounds(250 * x + 50, 100 * i + 50,
+								200, 50);
+						selectGame[x][i]
+								.addActionListener(new listenToGameButtons(
+										counter));
+						map1.add(selectGame[x][i], 0);
+						map1.repaint();
+						counter++;
+					}
+				}
+			}
+			stage = 2;
+			map1.repaint();
+		}
+	}
+
+	public void newGameButton() {
+		newOrOld = true;
+		initDone();
+		newGame.setVisible(false);
+		loadGame.setVisible(false);
+		map1.remove(newGame);
+		map1.remove(loadGame);
+		message = new JTextPane();
+		message.setText("Type the Name of Your World");
+		message.setBounds(main.screenWidth / 2 - 100,
+				main.screenHeight / 2 - 200, 200, 18);
+		message.setParagraphAttributes(center, true);
+		map1.add(message);
+		enterWorldName = new JTextPane();
+		enterWorldName.setBounds(main.screenWidth / 2 - 100,
+				main.screenHeight / 2 - 100, 200, 18);
+		enterWorldName.setParagraphAttributes(center, true);
+		map1.add(enterWorldName);
+		map1.repaint();
+		stage = 3;
+	}
+
+	public void startLoadScreen() {
+		if (selectGame != null) {
+			for (int i = 0; i < selectGame.length; i++) {
+				for (int x = 0; x < selectGame[i].length; x++) {
+					if (selectGame[i][x] != null) {
+						map1.remove(selectGame[i][x]);
+					}
+				}
+			}
+			selectGame = null;
+		}
+		if (message != null) {
+			map1.remove(message);
+			message = null;
+		}
+		if (enterWorldName != null) {
+			map1.remove(enterWorldName);
+			enterWorldName = null;
+		}
+		if (noSavedGames != null) {
+			map1.remove(noSavedGames);
+			noSavedGames = null;
+		}
+		if (done != null) {
+			map1.remove(done);
+			done = null;
+		}
+		newGame = new texturedButton("New/Overwrite Game", 200, 50);
+		newGame.setBounds(main.screenWidth / 2 - 300, 300, 200, 50);
+		map1.add(newGame, 0);
+		newGame.addActionListener(new listenToNewGame());
+		loadGame = new texturedButton("Load Game", 200, 50);
+		loadGame.setBounds(main.screenWidth / 2 + 100, 300, 200, 50);
+		map1.add(loadGame, 0);
+		loadGame.addActionListener(new listenToLoadGame());
+		back = new texturedButton("Back", 200, 50);
+		back.setBounds(main.screenWidth / 2 - 100, 500, 200, 50);
+		back.addActionListener(new listenToBack());
+		map1.add(back, 0);
+		map1.repaint();
+	}
 
 }
